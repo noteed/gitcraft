53999fccdd0689f49b03b595006a1a189ca0cb62
Your Name <you@example.com>
2019-01-16 23:26:27 +0100

    Replace Bezier curves by circle arcs.
    This is different, not really better. This should be an option.

--- Gitcraft.hs
+++ Gitcraft.hs
@@ -12,15 +12,17 @@ main = do
   mapM_ (putStrLn . uncurry renderArcs) commits'
 
 commits =
-  [ Commit "00000003" ["00000002"] (0, 0)
-  , Commit "00000002" ["00000001"] (0, 1)
-  , Commit "00000001" []           (0, 2)
+  [ Commit "00000003" ["00000002"] (1, 0)
+  , Commit "00000002" ["00000001"] (1, 1)
+  , Commit "00000001" []           (1, 2)
 
-  , Commit "00000004" ["00000002"] (1, 0)
+  , Commit "00000004" ["00000002"] (2, 0)
 
-  , Commit "00000005" ["00000002"] (2, 0)
+  , Commit "00000005" ["00000002"] (3, 0)
 
-  , Commit "00000006" ["00000002"] (3, 0)
+  , Commit "00000006" ["00000002"] (4, 0)
+
+  , Commit "00000007" ["00000002"] (0, 0)
   ]
 
 
@@ -63,25 +65,13 @@ renderCommit selection (Commit sha1 ps (x, y)) =
 renderArcs c cs = unlines (map (renderArc c) cs)
 
 renderArc (Commit _ _ (x1, y1)) (Commit _ _ (x2, y2)) | x1 > x2 = concat
-  [ "<path d=\"M"
-  , show x2'
-  , ","
-  , show (y2' - 8)
-  , " "
-  , "Q"
-  , show x2'
-  , ","
-  , show (fromIntegral y2' - fromIntegral (y2'- y1') / 2)
-  , " "
-  , show (fromIntegral x1' - fromIntegral (x1'- x2') / 2)
-  , ","
-  , show (fromIntegral y2' - fromIntegral (y2'- y1') / 2)
-  , " "
-  , "T"
-  , show x1'
-  , ","
-  , show (7 + y1')
-  , "\" fill=\"none\" stroke=\"blue\" />"
+  [ "<path d=\"M" ++ show x2' ++ "," ++ show (y2' - 7)
+  , " A30,30 0 0,1 " ++ show (x2' + 30) ++ "," ++ show (y2' - 30) ++"\""
+  , " style=\"stroke: blue; fill:none;\"/>"
+  , " <line stroke=\"blue\" x1=\"" ++ show (x2' + 30) ++ "\" y1=\"" ++ show (y2' - 30) ++ "\" x2=\"" ++ show (x1' - 30) ++ "\" y2=\"" ++ show (y1' + 30) ++ "\" />"
+  , " <path d=\"M" ++ show (x1' - 30) ++  "," ++ show (y1' + 30)
+  , " A30,30 0 0,0 " ++ show x1' ++ "," ++ show (y1' + 7) ++ "\""
+  , " style=\"stroke: blue; fill:none;\"/>"
   ]
   where
   x1' = 60 * x1 + 40
@@ -90,16 +80,12 @@ renderArc (Commit _ _ (x1, y1)) (Commit _ _ (x2, y2)) | x1 > x2 = concat
   y2' = 60 * y2 + 40
 
 renderArc (Commit _ _ (x1, y1)) (Commit _ _ (x2, y2)) =
-  "<use xlink:href=\"#" ++
-  xlink ++
-  "\" x=\"" ++
-  renderx x1 ++
-  "\" y=\"" ++
-  rendery y1 ++
-  "\" />"
+  "<line stroke=\"blue\" x1=\"" ++ show x1' ++ "\" y1=\"" ++ show (y1' + 7) ++ "\" x2=\"" ++ show x2' ++ "\" y2=\"" ++ show (y2' - 7) ++ "\" />"
   where
-  xlink | x1 == x2 = "arc"
-        | x1 > x2 = "left-arc"
+  x1' = 60 * x1 + 40
+  y1' = 60 * y1 + 40
+  x2' = 60 * x2 + 40
+  y2' = 60 * y2 + 40
 
 renderx x = show (60 * x + 40)
 rendery y = show (60 * y + 40)
